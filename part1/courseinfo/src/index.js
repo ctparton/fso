0b6fed80fb9d7f2d68caf13bad6e9c300b42a229
Callum Desktop <ctpartondev@gmail.com>
2020-06-21 21:48:46 +0100

    Split into components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Header courseName={course} />
+      <Content part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return(
+    <h1>{props.courseName}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+    <>
+      <Part part={props.part1} excercises={props.exercises1} />
+    </>
+    <>  
+      <Part part={props.part2} excercises={props.exercises2} />
+    </>
+    <>
+      <Part part={props.part3} excercises={props.exercises3} />
+    </>
+  </div>
+  )
+ 
+}
+
+const Total = (props ) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>{props.part} {props.excercises}</p>
+  )
+  
+}
+ReactDOM.render(<App />, document.getElementById('root'))
