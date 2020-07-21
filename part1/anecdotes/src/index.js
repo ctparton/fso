79f4f6ec3c228f9fbec964710149c53eab33d0b6
Callum Desktop <ctpartondev@gmail.com>
2020-07-21 16:47:07 +0100

    Added unicafe and anecdote apps for part 1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints]  = useState(Array(props.anecdotes.length).fill(0))
+  const generateRandom = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
+  
+  const voteForAnecdote = (selection) => {
+    console.log(selection)
+    const copy = [...points]
+    copy[selection] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]} has {points[selected]} votes</p>
+      <Button clickHandle={() => generateRandom()} text={"Next Anecdote"}></Button>
+      <Button clickHandle={() => voteForAnecdote(selected)} text={"Vote"}></Button>
+      <h1>Anecdote with the most votes</h1>
+      <p>{anecdotes[points.indexOf(Math.max(...points))]} with {points.indexOf(Math.max(...points))} v</p>
+    </div>
+  )
+}
+
+const Button = ({clickHandle, text}) => {
+  return (
+    <button onClick={clickHandle}>{text}</button>
+  ) 
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button clickHandle={() => incrementGood()} text={"Good"}></Button>
+      <Button clickHandle={() => incrementNeutral()} text = {"Neutral"}></Button>
+      <Button clickHandle={() => incrementBad()} text = {"Bad"}></Button>
+      
+      <Stats good={good} bad = {bad} neutral = {neutral}></Stats>
+    </div>
+  )
+}
+
+const Button = ({clickHandle, text}) => {
+  return (
+    <button onClick={clickHandle}>{text}</button>
+  ) 
+}
+
+const Stats = ({good, neutral, bad}) => {
+  const all = good + bad + neutral
+  if (all > 0) {
+    return(
+      <div>
+          <h2>Stats</h2>
+          <table>
+          <tbody>
+              <tr>
+                <td> <Statistic text="Good" value={good}></Statistic></td>
+              </tr>
+              <tr>
+                <td> <Statistic text="Neutral" value={neutral}></Statistic></td>
+              </tr>
+              <tr>
+                <td> <Statistic text="Bad" value={bad}></Statistic></td>
+              </tr>
+              <tr>
+                <td> <Statistic text="All" value={all}></Statistic></td>
+              </tr>
+              <tr>
+                <td> <Statistic text="Average" value={(good+ bad * -1) / 3}></Statistic></td>
+              </tr>
+              <tr>
+                <td> <Statistic text="Positive" value={good / all * 100}></Statistic></td>
+              </tr>
+              </tbody>
+          </table>
+      </div>)
+  }
+  return (
+    <div>
+      <p> No feedback given</p>
+    </div>
+  )
+}
+
+const Statistic = ({text, value}) => {
+  if (text.toLowerCase() === "positive") {
+    return (
+      <p>{text} {value}%</p>
+  )
+  }
+  return (
+      <p>{text} {value}</p>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
