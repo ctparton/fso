4a5ad218ba7293d59614c979d26bbe3d4c9b91d5
Callum Desktop <ctpartondev@gmail.com>
2020-07-26 16:33:21 +0100

    Implemented show, and added weather

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
   const [countries, setCountries] =  useState([])
-  const [viewToggle, setToggle] = useState(false)
+  const [currentCountry, setCurrent] = useState('')
 
   const fetchCountryDataHook = () => {
     axios
@@ -20,19 +17,19 @@ function App() {
   
   const handleSearch = (event) => {
     console.log(event.target.value)
-    setSearchTerm(event.target.value)
+    setCurrent(event.target.value)
   }
 
-  // works for show button but state gets stuck
-  const showHandler = (country, countriesToShow) => {
-    countriesToShow.filter(c => c.name === country.name)
-    setToggle(!viewToggle)
-  }
+  const countriesToShow = countries.filter(country => {
+    console.log(`the current selected country is ${currentCountry.length}`)
+    return country.name.toLowerCase().includes(currentCountry.toLowerCase()) })
+   
+  
 
   return (
     <div className="App">
-      <Search searchTerm={searchTerm} searchHandler={handleSearch}></Search>
-      <Countries countries={countries} searchTerm={searchTerm} showHandler={showHandler}></Countries>
+      <Search searchTerm={currentCountry} searchHandler={handleSearch} ></Search>
+      <Countries countries={countriesToShow} showHandler={setCurrent}></Countries>
     </div>
   );
 }
@@ -44,34 +41,53 @@ const Search = ({searchTerm, searchHandler}) => {
     </div>
   )
 }
-const Countries = ({countries, searchTerm, showHandler}) => {
-  const countriesToShow = countries.filter(country => {
-    return country.name.toLowerCase().includes(searchTerm.toLowerCase())
-  })
-    
+const Countries = ({countries, showHandler}) => {
   return (
     <div>
-      {countriesToShow.length > 10 
+      {countries.length > 10 
       ? <p>Too many matches, make search more specific</p>
-      : countriesToShow.length !== 1 
-      ? countriesToShow.map(country => <><p>{country.name}</p><button onClick={() =>showHandler(country, countriesToShow)}>show</button></>)
-      : <Country country={countriesToShow[0]} ></Country>}
+      : countries.length !== 1 
+      ? countries.map(country => <><p>{country.name}</p><button onClick={() => showHandler(country.name)}>show</button></>)
+      : <Country country={countries[0]} ></Country>}
     </div>
   )
 }
 
+const Weather = ({weather, city}) => {
+  console.log(weather)
+  if (!weather) {
+    return null
+  }
+   
+  return (
+    <div>
+      <h1>Weather in {city}</h1>
+      <p>Temperature {weather.temperature} &#8451; and {weather.weather_descriptions[0]} (last updated: {weather.observation_time})</p>
+      <img src={weather.weather_icons[0]} alt="Descriptive weather icon"></img>
+    </div> 
+  )
+}
+
 const Country = ({country}) => {
+    const [weather, setWeather] = useState(null) 
+    console.log()
+    const fetchWeatherDataHook = () => {
+      axios
+        .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`)
+        .then(response => setWeather(response.data.current))
+    }
+    useEffect(fetchWeatherDataHook, [])
     return (
       <div>
         <h1>{country.name}</h1>
-        <p>Capital {country.capital}</p> 
-        <p>Population {country.population}</p> 
-        <h2>Languages</h2>
+        <p><strong>Capital</strong> {country.capital}</p> 
+        <p><strong>Population</strong> {country.population.toLocaleString()}</p> 
+        <h2>Spoken Languages</h2>
         <ul>
         {country.languages.map(language => <li>{language.name}</li>)}
         </ul>
-        <img src={country.flag}  width="200px" height="200px"/>
-        
+        <img src={country.flag} alt={country.name + " flag"} width="200px" height="200px" padding="10px"/>
+        <Weather weather={weather} city={country.capital}/>
       </div>)
 }
 export default App;
