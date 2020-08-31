ab7722823fa905b306111b67a703e61a80982675
Callum Desktop <ctpartondev@gmail.com>
2020-08-31 23:18:52 +0100

    Added automatic login if user is still saved to browser storage

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -9,91 +9,99 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [notificationMessage, setNotificationMessage] = useState(null)
-  const blogRef = useRef()
+    const [blogs, setBlogs] = useState([])
+    const [user, setUser] = useState(null)
+    const [notificationMessage, setNotificationMessage] = useState(null)
+    const blogRef = useRef()
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs.sort((a,b) => b.likes - a.likes ))
-    )
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs( blogs.sort((a,b) => b.likes - a.likes ))
+        )
+    }, [])
 
-  const loginHandler = async (credentials) => {
-    try {
-      const response = await loginService.login(credentials)
-      window.localStorage.setItem('user', JSON.stringify(response))
-      setUser(response)
-      notifyUser({ text: `${response.name} is now logged in`, success: true })
-    } catch (error) {
-      notifyUser({ text: 'Username or password incorrect', success: false })
+    useEffect(() => {
+        const userAlreadyLogged = window.localStorage.getItem('user')
+        if (userAlreadyLogged) {
+            const user = JSON.parse(userAlreadyLogged)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
+    const loginHandler = async (credentials) => {
+        try {
+            const response = await loginService.login(credentials)
+            window.localStorage.setItem('user', JSON.stringify(response))
+            setUser(response)
+            notifyUser({ text: `${response.name} is now logged in`, success: true })
+        } catch (error) {
+            notifyUser({ text: 'Username or password incorrect', success: false })
+        }
     }
-  }
 
-  const handleLikes = async (blog) =>  {
-    const response = await blogService.likeBlog({ ...blog, likes: blog.likes + 1 })
-    setBlogs(blogs
-      .map(b => b.id === blog.id ? { ...b, likes: b.likes + 1 } : b)
-      .sort((a,b) => b.likes - a.likes))
-    console.log(response)
-  }
-
-  const handleDelete = async (blog)  => {
-    console.log(blog)
-    if (window.confirm(`Are you sure you want to delete ${blog.title}?`)) {
-      console.log(`User wishes to delete ${blog.id}`)
-      try {
-        blogService.setToken(JSON.parse(window.localStorage.getItem('user')).token)
-        const response = await blogService.deleteBlog(blog)
-        console.log(response)
+    const handleLikes = async (blog) =>  {
+        const response = await blogService.likeBlog({ ...blog, likes: blog.likes + 1 })
         setBlogs(blogs
-          .filter(b => b.id !== blog.id)
-          .sort((a,b) => b.likes - a.likes))
-        notifyUser({ text: `${blog.title} deleted successfully`, success: true })
-      } catch (error) {
-        notifyUser({ text: error.response.data.error, success: false })
-      }
-
+            .map(b => b.id === blog.id ? { ...b, likes: b.likes + 1 } : b)
+            .sort((a,b) => b.likes - a.likes))
+        console.log(response)
     }
-  }
 
-  const createBlogHandle = async (newBlog) => {
-    const data =  JSON.parse(window.localStorage.getItem('user'))
-    blogService.setToken(data.token)
-    try {
-      const response = await blogService.newBlog(newBlog)
-      setBlogs(blogs.concat(response))
-      notifyUser({ text: 'New blog created successfully', success: true })
-      blogRef.current.toggleVisibility()
-    } catch (error) {
-      notifyUser({ text: error.message, success: false })
+    const handleDelete = async (blog)  => {
+        console.log(blog)
+        if (window.confirm(`Are you sure you want to delete ${blog.title}?`)) {
+            console.log(`User wishes to delete ${blog.id}`)
+            try {
+                blogService.setToken(JSON.parse(window.localStorage.getItem('user')).token)
+                const response = await blogService.deleteBlog(blog)
+                console.log(response)
+                setBlogs(blogs
+                    .filter(b => b.id !== blog.id)
+                    .sort((a,b) => b.likes - a.likes))
+                notifyUser({ text: `${blog.title} deleted successfully`, success: true })
+            } catch (error) {
+                notifyUser({ text: error.response.data.error, success: false })
+            }
+
+        }
     }
-  }
 
-  const notifyUser = (message) => {
-    setNotificationMessage({ text: message.text, success: message.success })
-    setTimeout(() => setNotificationMessage(null), 5000)
-  }
-  return (
-    <div>
-      <h1>Bloglist: A list of your favourite blogs!</h1>
-      {<Notification message={notificationMessage}></Notification>}
-      {user === null ?
+    const createBlogHandle = async (newBlog) => {
+        const data =  JSON.parse(window.localStorage.getItem('user'))
+        blogService.setToken(data.token)
+        try {
+            const response = await blogService.newBlog(newBlog)
+            setBlogs(blogs.concat(response))
+            notifyUser({ text: 'New blog created successfully', success: true })
+            blogRef.current.toggleVisibility()
+        } catch (error) {
+            notifyUser({ text: error.message, success: false })
+        }
+    }
+
+    const notifyUser = (message) => {
+        setNotificationMessage({ text: message.text, success: message.success })
+        setTimeout(() => setNotificationMessage(null), 5000)
+    }
+    return (
         <div>
-          <LoginForm handleLogin={loginHandler}/>
-          <p>Please sign in to view blogs</p>
-        </div>
-        : <div>
-          <UserInfo name={user.name} logoutHandle={setUser}></UserInfo>
-          <Togglable buttonLabel="New blog" ref={blogRef}>
-            <CreateBlogForm newBlogHandle={createBlogHandle}></CreateBlogForm>
-          </Togglable>
+            <h1>Bloglist: A list of your favourite blogs!</h1>
+            {<Notification message={notificationMessage}></Notification>}
+            {user === null ?
+                <div>
+                    <LoginForm handleLogin={loginHandler}/>
+                    <p>Please sign in to view blogs</p>
+                </div>
+                : <div>
+                    <UserInfo name={user.name} logoutHandle={setUser}></UserInfo>
+                    <Togglable buttonLabel="New blog" ref={blogRef}>
+                        <CreateBlogForm newBlogHandle={createBlogHandle}></CreateBlogForm>
+                    </Togglable>
 
-          <Blogs blogs={blogs} likeHandler={handleLikes} deleteHandler={handleDelete}></Blogs>
-        </div>}
-    </div>
-  )
+                    <Blogs blogs={blogs} likeHandler={handleLikes} deleteHandler={handleDelete}></Blogs>
+                </div>}
+        </div>
+    )
 }
 
 export default App
